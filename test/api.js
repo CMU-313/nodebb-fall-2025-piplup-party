074b15d95f7ae4854014aed404e6f39fac89bde6
Connor Hyatt <connorhyatt1@gmail.com>
2025-10-09 23:18:19 +0000

    fix: add missing space before function parentheses in API test
    Fix ESLint error: Missing space before function parentheses
    This resolves the CI linting failure in GitHub Actions

--- test/api.js
+++ test/api.js
@@ -491,7 +491,7 @@ describe('API', async () => {
 					}
 				});
 
-				it('should not error out when called', async function() {
+				it('should not error out when called', async function () {
 					this.timeout(60000); // Increase timeout to 60 seconds for flaky test
 					await setupData();
 
